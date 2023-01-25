7fef7c22ee416e644c6551bd4089b1fd6647bdc2
Roberta <federicoroberta1@gmail.com>
2023-01-25 20:40:50 +0100

    esercizio rest and spread

--- es-63/exercise-5.js
+++
@@ -1,9 +0,0 @@
-const person = {
-  id: 1,
-  firstName: 'Mario',
-  lastName: 'Rossi',
-  age: 25
-};
-
-const { id, firstName, lastName, age } = person;
-console.log(id, firstName, lastName, age);
