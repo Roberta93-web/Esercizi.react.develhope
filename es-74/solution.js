b7fde420425ba2cf29467943779e8df925a0c64a
Roberta <federicoroberta1@gmail.com>
2023-02-07 13:31:19 +0100

    esercizio callback 3

--- es-74/solution.js
+++
@@ -1,7 +0,0 @@
-function repeatHello(callback) {
-  setInterval(callback, 1000);
-}
-
-const printHello = () => console.log("Hello");
-
-repeatHello(printHello);
--- es-75/exercise.js
+++ es-75/exercise.js
@@ -1,10 +1,9 @@
-function repeatHello(callback){
-    let intervalId = setInterval(callback, 1000)
+function repeatHello(callback) {
+    let intervalId = setInterval(callback, 1000);
 
     setTimeout(() => {
         clearInterval(intervalId);
-    }, 5000)
+    }, 5000);
 }
-
 
 repeatHello(() => console.log("Ciao"))
