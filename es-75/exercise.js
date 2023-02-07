08b98281359ee7cd64f3e33adef8c98a5b7704f2
Roberta <federicoroberta1@gmail.com>
2023-02-07 13:26:41 +0100

    esercizio callbak 3

---
+++ es-75/exercise.js
@@ -0,0 +1,10 @@
+function repeatHello(callback){
+    let intervalId = setInterval(callback, 1000)
+
+    setTimeout(() => {
+        clearInterval(intervalId);
+    }, 5000)
+}
+
+
+repeatHello(() => console.log("Ciao"))
