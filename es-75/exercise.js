99035e6f4fa81f037812d46cb9a1716598124e62
Roberta <federicoroberta1@gmail.com>
2023-02-08 15:11:43 +0100

    esercizio promise

--- es-75/exercise.js
+++
@@ -1,9 +0,0 @@
-function repeatHello(callback) {
-    let intervalId = setInterval(callback, 1000);
-
-    setTimeout(() => {
-        clearInterval(intervalId);
-    }, 5000);
-}
-
-repeatHello(() => console.log("Ciao"))
