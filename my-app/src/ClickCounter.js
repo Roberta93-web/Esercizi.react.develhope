d46a1e693a054be924be8309dfba80313a7f2a16
Roberta <federicoroberta1@gmail.com>
2023-03-02 15:52:39 +0100

    esercizio event 1

--- my-app/src/ClickCounter.js
+++ my-app/src/ClickCounter.js
@@ -1,22 +1,19 @@
 import React from "react";
-import Counter from "./Counter";
 
-export default class ClickCounter extends React.Component{
-    state = { count: 0}
-    handleButton = () => {
-      this.setState ((prevState) => {
-        return {count: state.count +1}
-    }}
-      }
-    })
+export default class ClickCounter extends React.Component {
+  state = { count: 0 };
+  handleButton = () => {
+    this.setState((prevState) => {
+      return { count: prevState.count + 1 };
+    });
+  };
 
-    }
-    render(){
-        return (
-            <div>
-                <button ></button>
-                <Counter />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleButton}>Increment</button>
+        <h1>{this.state.count}</h1>
+      </div>
+    );
+  }
 }
