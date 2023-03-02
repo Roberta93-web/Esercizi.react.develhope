b3674c7bf482642a2d4a5770ced5a3d751d155fa
Roberta <federicoroberta1@gmail.com>
2023-03-02 15:39:35 +0100

    esercizio lifecycle 1

--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -2,17 +2,16 @@ import React from "react";
 import { CounterDisplay } from "./CounterDisplay";
 
 export default class Counter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: this.props.initialValue,
-    };
-    setInterval(() => {
-      this.setState((state) => {
-        return { count: state.count + this.props.increment };
-      });
-    }, this.props.timeout);
-  }
+  componentDidMount(()=> {state = {
+    count: this.props.initialValue,
+  };
+  setInterval(() => {
+    this.setState((state) => {
+      return { count: state.count + this.props.increment };
+    });
+  }, this.props.timeout);
+}})
+   
   render() {
     return <CounterDisplay counter={this.state.count} />;
   }
