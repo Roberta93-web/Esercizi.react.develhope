0c1dc76e1ac29d29ea0072b9a578f3f68af892ac
Roberta <federicoroberta1@gmail.com>
2023-03-02 13:30:11 +0100

    esercizio conditional rendering 5

--- my-app/src/Age.js
+++ my-app/src/Age.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Age extends Component {
   render() {
     return (
-      <p>Your age is {this.props.age}</p>
-    )
+      <p>
+        {this.props.age >= 18
+          ? `Your age is ${this.props.age}`
+          : "You are very young"}
+      </p>
+    );
   }
 }
