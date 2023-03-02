aed197a25901018940787b5b4542e9b3717252bd
Roberta <federicoroberta1@gmail.com>
2023-03-02 13:22:49 +0100

    esercizio conditional rendering 4

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -6,9 +6,11 @@ export default class Welcome extends Component {
     return (
       <div>
         <p>Welcome, {this.props.name}!</p>
-        {this.props.age >= 18 && this.props.age <= 65 && (
-          <Age age={this.props.age} />
-        )}
+        {(
+          this.props.age >= 18 &&
+          this.props.age <= 65 &&
+          this.props.name === "John"
+        )(<Age age={this.props.age} />)}
       </div>
     );
   }
