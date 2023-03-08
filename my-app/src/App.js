50dcb3beb4f7482f423ec0533221ac1a2d0d5be9
Roberta <federicoroberta1@gmail.com>
2023-03-08 12:06:28 +0100

    esercizio forms 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,13 @@
 import React, { Component } from "react";
-import ClickCounter from "./ClickCounter";
-import Counter from "./Counter";
-import { CounterDisplay } from "./CounterDisplay";
 import Hello from "./Hello";
 import Login from "./Login";
-import Welcome from "./Welcome";
 
 export default class App extends Component {
-  onLogin = (state) => {
-    console.log(state);
-  };
   render() {
     return (
       <div>
         <Hello />
-        <Welcome name="John" age={30} />
-        <Counter initialValue={1} increment={1} timeout={1000} />
-        <CounterDisplay />
-        <ClickCounter />
-        <Login onLogin={this.onLogin} />
+        <Login />
       </div>
     );
   }
