b0bc91dfc4b0a0821baaa47a92f91f995bc93d85
Roberta <federicoroberta1@gmail.com>
2023-03-16 13:17:24 +0100

    esercizio function 3

--- my-app/src/Sum.js
+++ my-app/src/Sum.js
@@ -1,5 +1,5 @@
-export default function Sum({ numbers }) {
-  const sum = number.reduce((a, b) => a + b, 0);
+export default function Sum({ numbers = [1, 2, 5, 6, 7, 6] }) {
+  const sum = numbers.reduce((a, b) => a + b, 0);
 
   return (
     <div>
