c261fc2d36d0f9a5d160e26e2ccc13bb8cc9dc23
Roberta <federicoroberta1@gmail.com>
2023-03-16 15:34:10 +0100

    esercizio useEffect 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,6 @@ export default class App extends Component {
     return (
       <div>
         <Hello />
-        <Welcome name="John" age={30} />
         <Counter />
       </div>
     );
--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -1,18 +1,16 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
-export default class Counter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0,
-    };
-    setInterval(() => {
-      this.setState((state) => {
-        return { count: state.count + 1 };
-      });
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    let intervalId = setInterval(() => {
+      setCount((c) => c + 1);
     }, 1000);
-  }
-  render() {
-    return <h1>{this.state.count}</h1>;
-  }
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return <h1>{count}</h1>;
 }
