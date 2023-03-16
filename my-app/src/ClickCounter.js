75a16d398a38c45d9116a66381eb3094756caac1
Roberta <federicoroberta1@gmail.com>
2023-03-16 15:11:04 +0100

    esercizio useEffect 1

--- my-app/src/ClickCounter.js
+++ my-app/src/ClickCounter.js
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export default function ClickCounter({ initialValue = 0 }) {
+export default function ClickCounter({ initialValue = 0, onCounterChange }) {
   const [counter, setCounter] = useState(initialValue);
   function handleButton() {
     setCounter((c) => c + 1);
   }
+
+  useEffect(() => {
+    onCounterChange(counter);
+  }, [initialValue, onCounterChange]);
+
   return (
     <div>
       <button onClick={handleButton}>Increment</button>
