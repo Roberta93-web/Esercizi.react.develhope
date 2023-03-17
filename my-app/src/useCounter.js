8eed7566fa7a43eb7ad9457325db4be0b12bd92a
Roberta <federicoroberta1@gmail.com>
2023-03-17 15:53:53 +0100

    esercizio custom-hook-1

--- my-app/src/useCounter.js
+++ my-app/src/useCounter.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export function useCounter(initialValue = 5) {
+export function useCounter(initialValue = 0) {
   const [count, setCount] = useState(initialValue);
 
   function increment() {
