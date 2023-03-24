d5602e9bf9c8e6f268636f39ea1629b77813f31d
Roberta <federicoroberta1@gmail.com>
2023-03-24 15:06:06 +0100

    esercizio react router 4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,18 @@
 import { Link, Route, Routes } from "react-router-dom";
 import Welcome from "./Welcome";
 import Counter from "./Counter";
-
+import ShowGithunUser from "./ShowGithubUser";
 export function App() {
   return (
     <div className="border-solid border-2 border-sky-500">
       <div>
-        <Link to="/welcome">Welcome</Link> | <Link to="/counter">Counter</Link>
+        <Link to="/">Welcome</Link> | <Link to="/counter">Counter</Link> |{" "}
+        <Link to="/users/Roberta93-web">ShowGithubUser</Link>
       </div>
       <Routes>
         <Route path="/" element={<Welcome name="Roberta" />} />
         <Route path="/counter" element={<Counter />} />
-        <Route path="users/:username" element={<ShowGithubUser />}></Route>
+        <Route path="users/:username" element={<ShowGithunUser />}></Route>
       </Routes>
     </div>
   );
